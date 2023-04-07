587e5188ee295e7a335932b7f467504e9018899b
Sebastian00P <w64182@student.wsiz.edu.pl>
2023-04-07 17:24:58 +0200

    authorize classes view

--- FlowPanelApp/Controllers/ClassesController.cs
+++ FlowPanelApp/Controllers/ClassesController.cs
@@ -1,6 +1,7 @@
 ﻿using FlowPanelApp.Models;
 using FlowPanelApp.Services.ClassService;
 using FlowPanelApp.Services.SchoolService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -15,12 +16,14 @@ namespace FlowPanelApp.Controllers
         {
             _classService = classService;
         }
+        [Authorize]
         public async Task<IActionResult> Index(long schoolID)
         {
             SchoolId = schoolID;
             var model = await _classService.GetClassesBySchoolId(schoolID);
             return View(model);
         }
+        [Authorize]
         public IActionResult Create() 
         {
             return View();
